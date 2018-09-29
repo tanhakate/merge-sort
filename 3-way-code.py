bc04fef4dd42c00721e2c15877f59b7df691607f
Tanha Kate <31946411+tanhakate@users.noreply.github.com>
2018-09-29 18:57:47 +0900

    added extra conditions

--- 3-way-code.py
+++ 3-way-code.py
@@ -20,7 +20,6 @@ def merge_sort(array):
     merge_sort(left)
     merge_sort(center)
     merge_sort(right)
-
     merge(left, center, right, array)
 
 
@@ -28,40 +27,73 @@ def merge(left, center, right, a):
     l = len(left)
     c = len(center)
     r = len(right)
-    i = j = k = l = 0
+    i = j = k = z = 0 #z is index of the original list
 
-    while (i < l and j < r and k < c):
+    while (i < l and j < r and k < c): #After first iteration, l = 1, therefore we will no longer go into this while loop
         if left[i] <= right[j] and left[i] <= center[k]:
-            a[l] = left[i]
+            a[z] = left[i]
             i += 1
         elif center[k] <= right[j] and center[k] <= left[i]:
-            a[l] = center[k]
+            a[z] = center[k]
             k += 1
         else:
-            a[l] = right[j]
+            a[z] = right[j]
             j += 1
-        print(a[l])
-        l += 1
+
+        z += 1
+
+#cases where the second and third sublists have remaining values
+    while (k<c and j<r):
+        if center[k]<= right[j]:
+            a[z] = center[k]
+            k += 1
+        else:
+            a[z] = right[j]
+            j += 1
+        z += 1
+
+# cases where the first and third sublists have remaining values
+
+    while(i<l and j<r):
+        if left[i] <= right[j]:
+            a[z] = left[i]
+            i += 1
+        else:
+            a[z] = left[j]
+            j += 1
+        z += 1
+    # cases where the first and second sublists have remaining values
+
+    while (i < l and k < c):
+        if left[i] <= center[k]:
+            a[z] = left[i]
+            i += 1
+        else:
+            a[z] = center[k]
+            k += 1
+
+        z += 1
 
     # one sublist is added to the original array and there are left overs
     # there can be leftovers only in one sublist
 
     while (i < l):
-        a[l] = left[i]
+        a[z] = left[i]
         i += 1
-        l += 1
-
-    while (j < r):
-        a[l] = right[j]
-        j += 1
-        l += 1
+        z += 1
 
     while (k < c):
-        a[l] = center[k]
+        a[z] = center[k]
         k += 1
-        l += 1
+        z += 1
 
-my_array = [2,7,6,8,3,2,9,7,8]
+    while (j < r):
+        a[z] = right[j]
+        j += 1
+        z += 1
+
+
+
+my_array = [2,7,6,8,3,2,9,7,8,11]
 merge_sort(my_array)
-for i in my_array:
-    print (i,)
+print(my_array)
