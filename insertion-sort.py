c3999e831a0b95af353db7f7568bcf0c50309299
Tanha Kate <31946411+tanhakate@users.noreply.github.com>
2018-09-30 11:06:20 +0900

    uploaded insertion sort

---
+++ insertion-sort.py
@@ -0,0 +1,20 @@
+def insertion_sort(array):
+    for i in range (1,len(array)):
+
+        currentvalue = array[i]
+        position = i
+
+        while position>0 and currentvalue<array[position-1]:
+
+            array[position] = array[position-1]
+            position = position - 1
+
+        array[position] = currentvalue
+
+
+    return steps
+
+myArray = [54,26,93,17,77,31,44,55,20]
+insertion_sort(myArray)
+print(myArray)
+
